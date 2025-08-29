9e19dce4b83895d7f78982dda21d15fd026628a4
Callum-OP <callumoprey@gmail.com>
2025-08-29 22:42:05 +0100

    Improved SFML installation and added text to display

---
+++ README.md
@@ -0,0 +1,5 @@
+# How to run
+
+Open main.exe to run
+
+Command to compile: cl /EHsc /std:c++17 /I SFML-3.0.0/include main.cpp /link /LIBPATH:SFML-3.0.0/lib sfml-graphics.lib sfml-window.lib sfml-system.lib
--- main.cpp
+++ main.cpp
@@ -1,35 +1,41 @@
 #include <SFML/Graphics.hpp>
 #include <iostream>
+#include <optional>
 
 int main() {
-    sf::RenderWindow window(sf::VideoMode(800, 600), "NPC Chatbot");
-    sf::Font font;
-    font.loadFromFile("ARIAL.ttf");
+    sf::RenderWindow window(sf::VideoMode({800, 600}), "NPC Chatbot");
 
     // Create player
     sf::CircleShape player(50);
     player.setFillColor(sf::Color::Green);
-    player.setPosition(200, 250);
+    player.setPosition({200, 250});
 
     // Create NPC
     sf::CircleShape npc(50);
     npc.setFillColor(sf::Color::White);
-    npc.setPosition(500, 250);
-    // Center text on circle
-    sf::FloatRect textBounds = npcText.getLocalBounds();
-    npcText.setOrigin(textBounds.width / 2, textBounds.height / 2);
-    npcText.setPosition(npc.getPosition().x + npc.getRadius(),
-                        npc.getPosition().y + npc.getRadius() - 10); // Slightly above center
+    npc.setPosition({500, 250});
+
+    // Set up NPC text
+    sf::Font font("assets/fonts/arial.ttf");
+    sf::Text npcText(font, "Hello, there!", 20);
+    // Place text above NPC
+    npcText.setPosition({
+        npc.getPosition().x + npc.getRadius() - 80,
+        npc.getPosition().y + npc.getRadius() - 80
+    });
+
     while (window.isOpen()) {
-        sf::Event event;
-        while (window.pollEvent(event)) {
-            if (event.type == sf::Event::Closed)
+        while (auto eventOpt = window.pollEvent()) {
+            const sf::Event& event = *eventOpt;
+            if (event.is<sf::Event::Closed>()) {
                 window.close();
+            }
         }
 
         window.clear(sf::Color::Black);
         window.draw(player);
         window.draw(npc);
+        window.draw(npcText);
         window.display();
     }
 
