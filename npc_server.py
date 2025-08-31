e5cb2b753f20e80d5b971b58668e2d12575d47bb
Callum-OP <callumoprey@gmail.com>
2025-08-31 22:28:42 +0100

    Connected free online gpt-oss-20b to the Flask backend

--- npc_server.py
+++ npc_server.py
@@ -1,23 +1,49 @@
 from flask import Flask, request, jsonify
+from transformers import pipeline
+from openai import OpenAI
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 app = Flask(__name__)
 
-def generate_reply(message):
-    message = message.lower()
-    if "hello" in message:
-        return "Hello, there."
-    elif "good day" in message:
-        return "Yes it is a good day."
-    elif "bye" in message:
-        return "Farewell."
-    else:
-        return "I do not understand. Could you rephrase that?"
+# Set up gpt-oss-20b model
+client = OpenAI(
+    base_url="https://router.huggingface.co/v1",
+    api_key = os.getenv("HF_API_KEY") # Will need to log into hugging face and get a free api key and make sure no can access it
+)
+# Set up NPC personality
+messages=[
+        {"role": "system", "content": "You are a witty and friendly bystander in a large but empty world."},
+    ]
+response = client.chat.completions.create(
+    model="openai/gpt-oss-20b:fireworks-ai",
+    messages=messages
+)
 
+# List of messages over time
+messages = []
+
+# Function to get a reply from AI
+def generate_reply(message):
+    message = "hello"
+    message = message.lower()
+    messages.append({"role": "user", "content": message})
+    response = client.chat.completions.create(
+        model="openai/gpt-oss-20b:fireworks-ai",
+        messages=messages
+    )
+    print(response.choices[0].message.content)
+    return (response.choices[0].message.content)
+    
+# Send reply to frontend
 @app.route("/chat", methods=["POST"])
 def chat():
     data = request.get_json()
-    player_message = data.get("message", "")
-    reply = generate_reply(player_message)
+    message = data.get("message", "")
+    reply = generate_reply(message)
+    print(jsonify({"reply": reply}))
     return jsonify({"reply": reply})
 
 if __name__ == "__main__":
