738c96c676191ded8ec52809522ffbefca143ea0
Callum-OP <callumoprey@gmail.com>
2025-09-01 16:42:40 +0100

    Added personality to NPC

--- npc_server.py
+++ npc_server.py
@@ -13,13 +13,12 @@ client = OpenAI(
     base_url="https://router.huggingface.co/v1",
     api_key = os.getenv("HF_API_KEY") # Will need to log into hugging face and get a free api key and make sure no can access it
 )
-# Set up NPC personality
-messages=[
-        {"role": "system", "content": "You are a witty and friendly bystander in a large but empty world."},
-    ]
 
 # List of messages over time
-messages = []
+# Set up NPC personality
+messages=[
+        {"role": "system", "content": "Your name is James Ironside, you are a witty and friendly blacksmith in a large but empty world who prefers to speak with short responses."},
+    ]
 
 # Function to get a reply from AI
 def generate_reply(message):
@@ -29,6 +28,7 @@ def generate_reply(message):
         model="openai/gpt-oss-20b:fireworks-ai",
         messages=messages
     )
+    messages.append({"role": "assistant", "content": response.choices[0].message.content})
     return (response.choices[0].message.content)
     
 # Send reply to frontend
